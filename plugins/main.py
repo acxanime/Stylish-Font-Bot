8b64cb5ee3dc22d6e6a12c0a872fb7bc43f97b18
Techify Bots <TechifyBots@gmail.com>
2025-05-26 15:43:16 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -1,15 +1,13 @@
-import os
-import asyncio
+from asyncio import sleep
 from .fonts import Fonts
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-@Client.on_message(filters.private & filters.text & ~filters.command)
-async def style_buttons(c, m, cb=False):
-    text = m.text.strip()
-    if not text:
-        return
-        
+@Client.on_message(filters.private & filters.text)
+async def font_style_buttons(c, m: Message):
+    if not m.text or m.text.startswith("/"):
+        return  # Ignore empty or command messages
+
     buttons = [[
         InlineKeyboardButton('𝚃𝚢𝚙𝚎𝚠𝚛𝚒𝚝𝚎𝚛', callback_data='style+typewriter'),
         InlineKeyboardButton('𝕆𝕦𝕥𝕝𝕚𝕟𝕖', callback_data='style+outline'),
@@ -29,7 +27,7 @@ async def style_buttons(c, m, cb=False):
         ],[
         InlineKeyboardButton('𝘚𝘢𝘯𝘴', callback_data='style+slant'),
         InlineKeyboardButton('𝖲𝖺𝗇𝗌', callback_data='style+sim'),
-        InlineKeyboardButton('Ⓒ︎Ⓘ︎Ⓡ︎Ⓒ︎Ⓛ︎Ⓔ︎Ⓢ︎', callback_data='style+circles')
+        InlineKeyboardButton('Ⓒ︎Ⓘ︎Ⓡ︎Ⓒ︎Ⓛ︎Ⓔ︎Ⓢ︎', callback_data='style+circles'),
         ],[
         InlineKeyboardButton('🅒︎🅘︎🅡︎🅒︎🅛︎🅔︎🅢︎', callback_data='style+circle_dark'),
         InlineKeyboardButton('𝔊𝔬𝔱𝔥𝔦𝔠', callback_data='style+gothic'),
@@ -40,19 +38,55 @@ async def style_buttons(c, m, cb=False):
         InlineKeyboardButton('S̑̈ȃ̈d̑̈', callback_data='style+sad'),
         ],[
         InlineKeyboardButton('Next ➡️', callback_data="nxt")
-    ]]
+    ]]  
 
-    reply = await m.reply_text(text, reply_markup=InlineKeyboardMarkup(buttons), reply_to_message_id=m.id)
-    await asyncio.sleep(120)
+    reply = await m.reply_text(m.text, reply_markup=InlineKeyboardMarkup(buttons), reply_to_message_id=m.id)
+    await sleep(120)
+    try:
+        await m.delete()
+    except Exception:
+        pass
     try:
         await reply.delete()
-        await m.delete()
-    except:
+    except Exception:
         pass
 
-@Client.on_callback_query(filters.regex('^nxt'))
-async def nxt(c, m):
-    if m.data == "nxt":
+async def style_btn_back(c, m: CallbackQuery):
+    buttons = [[
+        InlineKeyboardButton('𝚃𝚢𝚙𝚎𝚠𝚛𝚒𝚝𝚎𝚛', callback_data='style+typewriter'),
+        InlineKeyboardButton('𝕆𝕦𝕥𝕝𝕚𝕟𝕖', callback_data='style+outline'),
+        InlineKeyboardButton('𝐒𝐞𝐫𝐢𝐟', callback_data='style+serif'),
+        ],[
+        InlineKeyboardButton('𝑺𝒆𝒓𝒊𝒇', callback_data='style+bold_cool'),
+        InlineKeyboardButton('𝑆𝑒𝑟𝑖𝑓', callback_data='style+cool'),
+        InlineKeyboardButton('Sᴍᴀʟʟ Cᴀᴘs', callback_data='style+small_cap'),
+        ],[
+        InlineKeyboardButton('𝓈𝒸𝓇𝒾𝓅𝓉', callback_data='style+script'),
+        InlineKeyboardButton('𝓼𝓬𝓻𝓲𝓹𝓽', callback_data='style+script_bolt'),
+        InlineKeyboardButton('ᵗⁱⁿʸ', callback_data='style+tiny'),
+        ],[
+        InlineKeyboardButton('ᑕOᗰIᑕ', callback_data='style+comic'),
+        InlineKeyboardButton('𝗦𝗮𝗻𝘀', callback_data='style+sans'),
+        InlineKeyboardButton('𝙎𝙖𝙣𝙨', callback_data='style+slant_sans'),
+        ],[
+        InlineKeyboardButton('𝘚𝘢𝘯𝘴', callback_data='style+slant'),
+        InlineKeyboardButton('𝖲𝖺𝗇𝗌', callback_data='style+sim'),
+        InlineKeyboardButton('Ⓒ︎Ⓘ︎Ⓡ︎Ⓒ︎Ⓛ︎Ⓔ︎Ⓢ︎', callback_data='style+circles'),
+        ],[
+        InlineKeyboardButton('🅒︎🅘︎🅡︎🅒︎🅛︎🅔︎🅢︎', callback_data='style+circle_dark'),
+        InlineKeyboardButton('𝔊𝔬𝔱𝔥𝔦𝔠', callback_data='style+gothic'),
+        InlineKeyboardButton('𝕲𝖔𝖙𝖍𝖎𝖈', callback_data='style+gothic_bolt'),
+        ],[
+        InlineKeyboardButton('C͜͡l͜͡o͜͡u͜͡d͜͡s͜͡', callback_data='style+cloud'),
+        InlineKeyboardButton('H̆̈ă̈p̆̈p̆̈y̆̈', callback_data='style+happy'),
+        InlineKeyboardButton('S̑̈ȃ̈d̑̈', callback_data='style+sad'),
+        ],[
+        InlineKeyboardButton('Next ➡️', callback_data="nxt")
+    ]]  
+    await m.message.edit_reply_markup(InlineKeyboardMarkup(buttons))
+
+
+async def nxt_fonts_nxt(c, m: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('🇸 🇵 🇪 🇨 🇮 🇦 🇱 ', callback_data='style+special'),
             InlineKeyboardButton('🅂🅀🅄🄰🅁🄴🅂', callback_data='style+squares'),
@@ -78,77 +112,93 @@ async def nxt(c, m):
             InlineKeyboardButton('S̶t̶r̶i̶k̶e̶', callback_data='style+strike'),
             InlineKeyboardButton('F༙r༙o༙z༙e༙n༙', callback_data='style+frozen')
             ],[
-            InlineKeyboardButton('⬅️ Back', callback_data='nxt+0')
-        ]]
-        await m.answer()
+            InlineKeyboardButton('⬅️ Back', callback_data='fontblack')
+        ]]        
         await m.message.edit_reply_markup(InlineKeyboardMarkup(buttons))
-    else:
-        await style_buttons(c, m, cb=True)
 
 
-@Client.on_callback_query(filters.regex('^style'))
-async def style(c, m):
-    await m.answer()
-    cmd, style = m.data.split('+')
-
-    # Font style selection (you could also use a dict to simplify this)
-    cls = getattr(Fonts, {
-        'typewriter': 'typewriter',
-        'outline': 'outline',
-        'serif': 'serif',
-        'bold_cool': 'bold_cool',
-        'cool': 'cool',
-        'small_cap': 'smallcap',
-        'script': 'script',
-        'script_bolt': 'bold_script',
-        'tiny': 'tiny',
-        'comic': 'comic',
-        'sans': 'sans',
-        'slant_sans': 'slant_san',
-        'slant': 'slant',
-        'sim': 'sim',
-        'circles': 'circles',
-        'circle_dark': 'dark_circle',
-        'gothic': 'gothic',
-        'gothic_bolt': 'bold_gothic',
-        'cloud': 'cloud',
-        'happy': 'happy',
-        'sad': 'sad',
-        'special': 'special',
-        'squares': 'square',
-        'squares_bold': 'dark_square',
-        'andalucia': 'andalucia',
-        'manga': 'manga',
-        'stinky': 'stinky',
-        'bubbles': 'bubbles',
-        'underline': 'underline',
-        'ladybug': 'ladybug',
-        'rays': 'rays',
-        'birds': 'birds',
-        'slash': 'slash',
-        'stop': 'stop',
-        'skyline': 'skyline',
-        'arrows': 'arrows',
-        'qvnes': 'rvnes',
-        'strike': 'strike',
-        'frozen': 'frozen'
-    }[style], None)
-
-    if not cls:
-        return await m.message.reply("Invalid style")
-
+async def style_btn_editz(c, m, style):
+    if style == 'typewriter':
+        cls = Fonts.typewriter
+    if style == 'outline':
+        cls = Fonts.outline
+    if style == 'serif':
+        cls = Fonts.serif
+    if style == 'bold_cool':
+        cls = Fonts.bold_cool
+    if style == 'cool':
+        cls = Fonts.cool
+    if style == 'small_cap':
+        cls = Fonts.smallcap
+    if style == 'script':
+        cls = Fonts.script
+    if style == 'script_bolt':
+        cls = Fonts.bold_script
+    if style == 'tiny':
+        cls = Fonts.tiny
+    if style == 'comic':
+        cls = Fonts.comic
+    if style == 'sans':
+        cls = Fonts.sans
+    if style == 'slant_sans':
+        cls = Fonts.slant_san
+    if style == 'slant':
+        cls = Fonts.slant
+    if style == 'sim':
+        cls = Fonts.sim
+    if style == 'circles':
+        cls = Fonts.circles
+    if style == 'circle_dark':
+        cls = Fonts.dark_circle
+    if style == 'gothic':
+        cls = Fonts.gothic
+    if style == 'gothic_bolt':
+        cls = Fonts.bold_gothic
+    if style == 'cloud':
+        cls = Fonts.cloud
+    if style == 'happy':
+        cls = Fonts.happy
+    if style == 'sad':
+        cls = Fonts.sad
+    if style == 'special':
+        cls = Fonts.special
+    if style == 'squares':
+        cls = Fonts.square
+    if style == 'squares_bold':
+        cls = Fonts.dark_square
+    if style == 'andalucia':
+        cls = Fonts.andalucia
+    if style == 'manga':
+        cls = Fonts.manga
+    if style == 'stinky':
+        cls = Fonts.stinky
+    if style == 'bubbles':
+        cls = Fonts.bubbles
+    if style == 'underline':
+        cls = Fonts.underline
+    if style == 'ladybug':
+        cls = Fonts.ladybug
+    if style == 'rays':
+        cls = Fonts.rays
+    if style == 'birds':
+        cls = Fonts.birds
+    if style == 'slash':
+        cls = Fonts.slash
+    if style == 'stop':
+        cls = Fonts.stop
+    if style == 'skyline':
+        cls = Fonts.skyline
+    if style == 'arrows':
+        cls = Fonts.arrows
+    if style == 'qvnes':
+        cls = Fonts.rvnes
+    if style == 'strike':
+        cls = Fonts.strike
+    if style == 'frozen':
+        cls = Fonts.frozen 
+    r, oldtxt = m.message.reply_to_message.text.split(None, 1) 
+    new_text = cls(oldtxt)            
     try:
-        r, oldtxt = m.message.reply_to_message.text.split(None, 1)
-        new_text = cls(oldtxt)
-
-        await m.message.edit_text(
-            f"`{new_text}`\n\n👆 Click To Copy", 
-            reply_markup=m.message.reply_markup
-        )
-
-        await asyncio.sleep(120)
-        await m.message.delete()
-        await m.message.reply_to_message.delete()
-
+        await m.message.edit_text(f"`{new_text}`\n\n👆 Click To Copy", reply_markup=m.message.reply_markup)
     except Exception as e:
         print(e)
