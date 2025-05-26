a4dd7c8323451c4c3f55ca891217666c635e4b9d
Techify Bots <TechifyBots@gmail.com>
2025-05-26 15:44:05 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -42,13 +42,12 @@ async def callback_query_handler(client, query: CallbackQuery):
     elif data == "close":
         await query.message.delete()
 
-    elif query.data.startswith("next_") or query.data.startswith("back_"):
-        _, current_page, text_id = query.data.split("_", 2)  # Get the actual text_id
-        if text_id not in font_text_cache:
-            return await query.message.edit_text("❗️Your session has expired. Please send /font again.")
-        user_text = font_text_cache[text_id]  # Retrieve original text
-        new_page = int(current_page) + 1 if query.data.startswith("next") else int(current_page) - 1
-        try:
-            await send_fonts_page(client, query.message, page=new_page, text_id=text_id, user_text=user_text, edit=True)
-        except Exception as e:
-            print("Edit error:", e)
+    elif query.data == "nxt":
+       await nxt_fonts_nxt(client, query)
+
+    elif query.data == "fontblack":
+       await style_btn_back(client, query)
+
+    elif "style" in query.data:
+       cmd, style = query.data.split('+')
+       await style_btn_editz(client, query, style)
